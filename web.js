d776e3128c25d55d2592de492ef24cf4aea4b995
Thom Seddon <thom@seddonmedia.co.uk>
2013-05-01 20:45:35 +0100

    Make it work

--- web.js
+++ web.js
@@ -1,13 +1,33 @@
-var express = require('express');
+var express = require('express'),
+	jsdom = require('jsdom');
 
 var app = express();
 
-app.get('/', function (req, res) {
-	res.json([{
-		"name": "MancJS",
-		"venue": "TechHub",
-		"details": "..."
-	}]);
-});
+var serve = function (list) {
 
-app.listen(process.env.PORT || 3000);
+	app.get('/', function (req, res) {
+		res.jsonp(list);
+	});
+
+	app.listen(process.env.PORT || 3000);
+	console.log('serving on ' + (process.env.PORT || 3000))
+};
+
+jsdom.env({
+	html: 'http://manchester.techhub.com/events/',
+	scripts: ["http://code.jquery.com/jquery.js"],
+	done: function (errors, window) {
+		var $ = window.$;
+
+		var list = [];
+		$('article.card.article.span6.event').each(function() {
+			list.push({
+				name: $(this).find('.event_title a').html(),
+				venue: $(this).find('.event_venue').html(),
+				description: $(this).find('.event_description p').last().html().replace(/\n/g, ' ')
+			});
+		});
+
+		serve(list);
+	}
+});
