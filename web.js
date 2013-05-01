7f0e99c3f1686fa0406b763c65dd14cf4e6d3c95
Thom Seddon <thom@seddonmedia.co.uk>
2013-05-01 20:48:37 +0100

    Clean ups

--- web.js
+++ web.js
@@ -9,18 +9,21 @@ var serve = function (list) {
 		res.jsonp(list);
 	});
 
-	app.listen(process.env.PORT || 3000);
-	console.log('serving on ' + (process.env.PORT || 3000))
+	var port = process.env.PORT || 3000;
+	app.listen(port);
+	console.log('serving on ' + port);
 };
 
 jsdom.env({
 	html: 'http://manchester.techhub.com/events/',
 	scripts: ["http://code.jquery.com/jquery.js"],
 	done: function (errors, window) {
+		console.log('processing....');
+
 		var $ = window.$;
 
 		var list = [];
-		$('article.card.article.span6.event').each(function() {
+		$('article.card.article.span6.event').each(function () {
 			list.push({
 				name: $(this).find('.event_title a').html(),
 				venue: $(this).find('.event_venue').html(),
